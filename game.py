c302acabad7a3571b527cca20592ac2795a038b7
caitevanish <cait.evan@gmail.com>
2022-01-06 14:11:50 -0800

    In game.py, completed and tested display_welcome and game_type methods. Added new method: single_player_game

--- game.py
+++ game.py
@@ -2,13 +2,28 @@ class Game:
     def __init__(self):
         pass
 
-    def display_welcome(self):
-        print('Welcome to Rock Paper Scissors Lizard Spock')
+    def display_welcome(self): #tested
+        print('Welcome to Rock Paper Scissors Lizard Spock! The greatest game you only heard of and always wanted to play.\n\nThe rules are similar to Rock, Paper, Scissors, but with an added twist: Lizard and Spock!\n')
+
+        print('Here’s how to win (and lose): \n\nRock crushes Scissors\nScissors cuts Paper\nPaper covers Rock\nRock crushes Lizard\nLizard poisons Spock\nSpock smashes Scissors\nScissors decapitates Lizard\nLizard eats Paper\nPaper disproves Spock\nSpock vaporizes Rock\n')
+
+        print('Winning a round will give you one point.\nThe goal is to have 2 wins to beat your opponent.\n')
+
+    def game_type(self): #tested
+        user_input = int(input('Type [1] for a single player game.\nType [2] for a double player game.\n'))
+        if user_input == 1 or 2: #temporary
+            self.single_player_game()
+        print('Let\'s Begin!')
+
+
+
+    def single_player_game():
         pass
 
-    def display_rules(self):
-        pass
+        
 
     def run_game(self):
         self.display_welcome()
+        self.game_type()
+
     
